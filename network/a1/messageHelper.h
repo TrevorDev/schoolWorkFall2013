308024645b887b0676249a586048f85ae8578bf4
Trevor Baron <trevorjbaron@gmail.com>
2013-10-01 20:04:32 -0400

    done a1

--- network/a1/messageHelper.h
+++ network/a1/messageHelper.h
@@ -1,10 +1,11 @@
 /********
 messageHelper.h -- header file for messageHelper.c
+author: Trevor Baron
 ********/
 #ifndef MHELPER_H
 #define MHELPER_H 1
 
-#define MAX_BUF 30
+#define MAX_BUF 32
 void makePipes(char * children[], int size);
 void deletePipes (char * children[], int size);
 void sendMessage(char * message, char * link);
--- network/a1/n1.c
+++ network/a1/n1.c
@@ -8,6 +8,8 @@
 
 int main()
 {
+
+	//Create children pipes
 	char * children[2];
 	children[0]="link1";
 	children[1]="link2";
@@ -17,23 +19,26 @@ int main()
 	int running = 1;
 	while(running){
 		char * message = (char *)calloc(MAX_BUF, sizeof(char));;
-		printf("Enter your message\n");
+		printf("Enter your message or q to quit all\n");
 		fgets(message, sizeof(char)*MAX_BUF, stdin);
 		if(message[strlen(message)-1]=='\n'){
 			message[strlen(message)-1]='\0';
 		}
 
+		//Send message to child nodes
 		for(int i=0;i<size;i++){
 			sendMessage(message, children[i]);
 		}
+
+		//Receive done message from child nodes
 		for(int i=0;i<size;i++){
 			char * recMessage = getMessage(children[i]);
-			//printf("%s, received %s from %s\n","n1",recMessage,children[i]);
 			free(recMessage);
 		}
 
 		printf("all messages received\n");
 
+		//Quit once all children are closed
 		if(strcmp(message, "q")==0){
 	    	running=0;
 	    }
