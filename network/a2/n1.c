6bc88056dd3ddf1b7c7eba6f833125e4d45ce97f
Trevor Baron <trevorjbaron@gmail.com>
2013-10-16 23:11:49 -0400

    add a2 more

--- network/a2/n1.c
+++ network/a2/n1.c
@@ -19,6 +19,7 @@ int main()
 	int running = 1;
 	while(running){
 		char * message = (char *)calloc(MAX_BUF, sizeof(char));
+		char * toSend = (char *)calloc(MAX_BUF, sizeof(char));
 		printf("Enter your file and node to send to eg. file.txt n4\n");
 		fgets(message, sizeof(char)*MAX_BUF, stdin);
 
@@ -27,7 +28,33 @@ int main()
 		}
 		char*file = strtok(message," ");
 		char*toNode = strtok(NULL," ");
+		char*charPacket = calloc(MAX_BUF,sizeof(char));
+		if(toNode!=NULL){
+			charPacket[0]='T';
+			for(int i=0;i<strlen(toNode);i++){
+				charPacket[i+1]=toNode[i];
+				charPacket[i+2]='\0';
+			}
+			printf("TO: %s\n",charPacket);
+		}
 		printf("%s-%s\n",file,toNode);
+		FILE * fd = fopen(file, "r");
+		if(fd!=NULL){
+			 while(fgets(toSend, 25, fd)!=NULL ) {
+				printf("%s",toSend);
+				for(int i=0;i<size;i++){
+					for(int j=0;j<size;j++){
+						charPacket[0]=toSend[j];
+						sendMessage(charPacket, children[i]);
+					}
+				}
+			 }
+			 printf("\n");
+			 
+		}else{
+			printf("Invalid File\n");
+		}
+
 		/*//Send message to child nodes
 		for(int i=0;i<size;i++){
 			sendMessage(message, children[i]);
@@ -46,6 +73,8 @@ int main()
 	    	running=0;
 	    }
 	    free(message);
+	    free(charPacket);
+	    free(toSend);
 	}
 
 	deletePipes(children,size);
